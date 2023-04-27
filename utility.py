5e72dcd5ba9e03150b75cd21f285170d72d6bc6d
awill139 <awill13913@gmail.com>
2023-04-27 13:41:10 -0400

    adding torch stuff

--- utility.py
+++ utility.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import cv2
+import torchvision.transforms as transforms
 
 def img_to_encoding(image_path, model):
     img1 = cv2.imread(image_path, -1)
@@ -15,3 +16,20 @@ def resize_img(image_path):
     img = cv2.imread(image_path, 1)
     img = cv2.resize(img, (96, 96))
     cv2.imwrite(image_path, img)
+
+def trans_img(img):
+
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+    ])
+
+    img1 = cv2.imread(img, -1)
+    img = img1[...,::-1]
+    img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
+    img = np.array([img])
+    img = transform(img)
+    return img
